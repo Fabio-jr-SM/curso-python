0e2dc2b57654554f857666909ac4696b35829b74
Fabio Junior <91484736+Fabio-jr-SM@users.noreply.github.com>
2023-10-06 07:51:02 -0400

    Update cadastroUsuario.py

--- cadastroUsuario.py
+++ cadastroUsuario.py
@@ -1,9 +1,38 @@
 from datetime import datetime
 import os
 
-os.system("cls")
+
+global ident
+global ende
+
+
+class Identificacao:
+    def __init__(self, name, data_nascimento_str, titulo,sx,naturalidade,nome_mae):
+        self.name = name
+        self.data_nascimento_str = data_nascimento_str
+        self.titulo = titulo
+        self.sx = sx
+        self.naturalidade = naturalidade
+        self.nome_mae = nome_mae
+
+
+class Endereco:
+   def __init__(self,cep, municipio, uf, logradouro, endereco, numero_residencia, ddd, numero_telefone, numero_celular, complemento, bairro ):
+       self.cep = cep
+       self.municipio = municipio
+       self.uf = uf
+       self.logradouro = logradouro
+       self.endereco = endereco
+       self.numero_residencia = numero_residencia
+       self.ddd = ddd
+       self.numero_telefone = numero_telefone
+       self.numero_celular = numero_celular
+       self.complemento = complemento
+       self.bairro = bairro
+
 
 def identificacao():
+    global ident
     while True:
         name = input("Nome: ")
         if len(name) > 3:
@@ -11,6 +40,7 @@ def identificacao():
             break
         else:
             print("Nome deve ter mais de 3 caracteres.\n")
+
 
     while True:
         data_nascimento_str = input("Digite a data de nascimento dd/mm/aaaa: ")
@@ -20,7 +50,7 @@ def identificacao():
             break
         except ValueError:
             print("Data de nascimento inválida. Certifique-se de usar o formato dd/mm/aaaa.")
-    
+   
     while True:
         titulo = input("Titulo de eleitor: ")
         if len(titulo) == 12:
@@ -28,7 +58,7 @@ def identificacao():
             break
         else:
             print("Titulo de eleitor invalido.\n")
-    
+   
     while True:
         sx = input("Digite seu Sexo (f/m): ")
         if sx == 'f' or sx == 'm':
@@ -36,7 +66,7 @@ def identificacao():
             break
         else:
             print("Sexo deve ser 'f' ou 'm'.\n")
-    
+   
     while True:
         naturalidade = input("Naturalidade: ")
         if len(naturalidade)>2:
@@ -44,19 +74,37 @@ def identificacao():
             break
         else:
             print("Naturalidade invalida.\n")
-            
+           
     while True:
         nome_mae = input("Nome da mãe: ")
-        if len(naturalidade)>3:
+        if len(nome_mae)>3:
             print("nome da mãe válido!\n")
             break
         else:
             print("nome da mae invalida.\n")
+   
+    ident = Identificacao(name, data_nascimento_str, titulo,sx,naturalidade,nome_mae)
+    os.system("cls")
 
 
+    print("\n\nDados de Identificação:\n")
+    print("Nome: ", ident.name,"\n")
+    print("Data de Nacimento: ", ident.data_nascimento_str,"\n")
+    print("Titulo: ", ident.titulo,"\n")
+    print("Sexo: ", ident.sx,"\n")
+    print("Naturalidade: ", ident.naturalidade,"\n")
+    print("Nome da Mãe: ", ident.nome_mae,"\n")
+
+
+    op = input("\nDeseja confirmar seus dados? [S][N]: ")
+
+
+    if(op == "N"):
+        identificacao()
 #########################################################################
 
 def endereco():
+    global ende
     while True:
         cep = input("CEP: ")
         if len(cep) == 8:
@@ -72,7 +120,7 @@ def endereco():
             break
         else:
             print("municipio invalido.\n")
-    
+   
     while True:
         uf = input("UF: ")
         if len(uf) == 2:
@@ -80,7 +128,7 @@ def endereco():
             break
         else:
             print("UF invalido.\n")
-    
+   
     while True:
         logradouro = input("logradouro: ")
         if len(logradouro) > 2:
@@ -88,7 +136,7 @@ def endereco():
             break
         else:
             print("logradouro invalido.\n")
-    
+   
     while True:
         endereco = input("Endereco: ")
         if len(endereco)>5:
@@ -96,7 +144,7 @@ def endereco():
             break
         else:
             print("Endereco invalida.\n")
-            
+           
     while True:
         numero_residencia = input("Numero da residencia: ")
         if len(numero_residencia)>0:
@@ -104,7 +152,7 @@ def endereco():
             break
         else:
             print("Numero de residencia invalido.\n")
-            
+           
     while True:
         ddd = input("DDD: ")
         if len(ddd)==3:
@@ -112,6 +160,7 @@ def endereco():
             break
         else:
             print("DDD invalido.\n")
+
 
     while True:
         numero_telefone =input("Numero Telefone: ")
@@ -121,6 +170,7 @@ def endereco():
         else:
             print("Numero de telefone invalido.\n")
 
+
     while True:
         numero_celular = input("Numero celular: ")
         if len(numero_celular)==9:
@@ -128,6 +178,7 @@ def endereco():
             break
         else:
             print("Numero de celular invalido.\n")
+
 
     while True:
         complemento = input("complemento: ")
@@ -137,6 +188,7 @@ def endereco():
         else:
             print("complemento invalida.\n")
 
+
     while True:
         bairro = input("bairro: ")
         if len(bairro)>3:
@@ -145,8 +197,53 @@ def endereco():
         else:
             print("bairro invalida.\n")
 
-input("--------IDENTIFICAÇÃO--------\n")
+    ende = Endereco(cep, municipio, uf, logradouro, endereco, numero_residencia, ddd, numero_telefone, numero_celular, complemento, bairro)
+    os.system("cls")
+
+
+    print("\n\nDados de endereço:")
+    print("Cep: ", ende.cep)
+    print("Municipio: ", ende.municipio)
+    print("Uf: ", ende.uf)
+    print("Logradouro: ", ende.logradouro)
+    print("EndereÇo: ", ende.endereco)
+    print("Numero de residència: ", ende.numero_residencia)
+    print("ddd: ", ende.ddd)
+    print("Numero de telefone: ", ende.numero_telefone)
+    print("Numero de celular: ", ende.numero_celular)
+    print("Complemento: ", ende.complemento)
+    print("Bairro: ", ende.bairro)
+
+
+    op = input("\nDeseja confirmar seus dados? [S][N]")
+
+
+    if(op == "N" ):
+        endereco()
+
+
+print("--------IDENTIFICAÇÃO--------\n")
 identificacao()
 
-input("\n--------ENDEREÇO--------\n")
+
+print("\n--------ENDEREÇO--------\n")
 endereco()
+
+
+print("\n\nDados de Identificação:\n")
+print("Nome: ", ident.name,"\n")
+print("Data de Nacimento: ", ident.data_nascimento_str,"\n")
+print("Titulo: ", ident.titulo,"\n")
+print("Sexo: ", ident.sx,"\n")
+print("Naturalidade: ", ident.naturalidade,"\n")
+print("Nome da Mãe: ", ident.nome_mae,"\n")
+
+
+print("\n\nDados de endereço:")
+print("Cep: ", ende.cep,"\n")
+print("Municipio: ", ende.municipio,"\n")
+print("Uf: ", ende.uf,"\n")
+print("Logradouro: ", ende.logradouro,"\n")
+print("EndereÇo: ", ende.endereco,"\n")
+print("Numero de residencia: ", ende.numero_residencia,"\n")
+print("ddd: ", ende.ddd,"\n")
