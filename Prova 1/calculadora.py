8638fc132bb653dd4ac3bb1e9cd29f3589e010b2
Fabio Junior <91484736+Fabio-jr-SM@users.noreply.github.com>
2023-10-11 09:28:55 -0400

    Update calculadora.py
    Adicionou uma condição de O resultado de operações anteriores fica na tela e será usado em qualquer outra operação a menos que o usuário escolha a opção 0 - Zerar resultado.

--- Prova 1/calculadora.py
+++ Prova 1/calculadora.py
@@ -1,6 +1,3 @@
-import os
-os.system("cls")
-
 #Realiza as operações usando uma função nativa do python 
 soma = lambda a,b: a+b
 subtracao = lambda a,b: a-b
@@ -11,6 +8,7 @@ divisao = lambda a,b: a/b
 zerar=0
 cont =0
 vetOP=[] #armazena os valores das operações
+opAnt="nao"
 
 #inicia um laço de repetição que só irá sair quando op=='S' com um break
 while True:
@@ -24,35 +22,55 @@ while True:
     # usuario escolhe uma operação da calcularoda
     op = input("\nDigite uma opção:\n(+) Soma\n(-) Subtração\n(*) Multiplicação\n(/) Divisão\n(0) Zerar resultado\n(S) Sair\n")
     
+    if(zerar!=0 and op!='0'):
+        opAnt = input("Deseja Fazer essa operação usando o resultado de outra operação? [sim][nao]: ")
+
     #condições para fazer a operação que o usuario escolheu
+    
     if(op=='+'):
         a = float(input("Digite o Primeiro Numero: "))
-        b = float(input("Digite o Segundo Numero: "))
+        if opAnt =="nao":
+            b = float(input("Digite o Segundo Numero: "))
+        elif opAnt =="sim":
+            opEscolhida=int(input("Escolha a operação atenrior: [1,n]"))
+            b = vetOP[opEscolhida-1]
         print(soma(a,b))
         vetOP.append(soma(a,b)) #adiciona o valor da operação no vetor
         
         zerar=1 #variavel zerar recebe um valor diferente de 0 para imprimir para o usuario as operações anteriores
     elif(op=='-'):
         a = float(input("Digite o Primeiro Numero: "))
-        b = float(input("Digite o Segundo Numero: "))
+        if opAnt =="nao":
+            b = float(input("Digite o Segundo Numero: "))
+        elif opAnt =="sim":
+            opEscolhida=int(input("Escolha a operação atenrior: [1,n]"))
+            b = vetOP[opEscolhida-1]
         print(subtracao(a,b))
         vetOP.append(subtracao(a,b))
     
         zerar=1
     elif(op=='*'):
         a = float(input("Digite o Primeiro Numero: "))
-        b = float(input("Digite o Segundo Numero: "))
+        if opAnt =="nao":
+            b = float(input("Digite o Segundo Numero: "))
+        elif opAnt =="sim":
+            opEscolhida=int(input("Escolha a operação atenrior: [1,n]"))
+            b = vetOP[opEscolhida-1]
         print(multiplicacao(a,b))
         vetOP.append(multiplicacao(a,b))
         
         zerar=1
     elif(op=='/'): #condição para divisão
         a = float(input("Digite o Primeiro Numero: "))
-        b = float(input("Digite o Segundo Numero (diferente de zero): "))
+        if opAnt =="nao":
+            b = float(input("Digite o Segundo Numero: "))
+        elif opAnt =="sim":
+            opEscolhida=int(input("Escolha a operação atenrior: [1,n]"))
+            b = vetOP[opEscolhida-1]
 
         if(b==0): #caso para que o usuario digite um divisor diferente de zero
-           
-           while True: #roda loop enquanto o divisor não é diferente de zero
+        
+            while True: #roda loop enquanto o divisor não é diferente de zero
                 print("Try again!\n")
                 b = float(input("Digite o Segundo Numero (diferente de zero): "))
                 if(b!=0):
@@ -66,8 +84,8 @@ while True:
         zerar=0
         cont=0
         vetOP = [] #declara o mesmo vetor como vazio
+        opAnt="nao"
     elif(op=='S'): #caso para sair quando a opção for S
         break
     else: #caso de opção invalida
         print("Opção Invalida!!\n")
-        
